3bf6e7b23a8dc4ee65b4955385ee7dc611d327a7
Giov_cp <giovannicupito@gmail.com>
2022-10-21 15:12:43 +0200

    modifica test

--- gateway/urls.py
+++ gateway/urls.py
@@ -1,3 +1,3 @@
-GATEWAY_URL = 'http://gateway:5001'     # add <PORT1>
+GATEWAY_URL = 'http://gg:5001'     # add <PORT1>
 MATH_URL = 'http://math_py:5003'        # add <PORT2>
 STRING_URL = 'http://string_rust:5000'  # add <PORT3>
